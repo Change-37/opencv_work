403070f24c53abfd80283ea3ce2c8f4aeab337bd
Change-37 <changeunp04@gmail.com>
2023-03-31 23:06:15 +0900

    20230331

---
+++ nocchi.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+img1 = cv.imread('images/20230331_1.jpg')
+img2 = cv.imread('images/20230331_2.jpg')
+
+img3 = cv.subtract(img1, img2)
+
+img4 = cv.absdiff(img1, img2)
+
+title = ['src', 'map', 'sub', 'abs']
+imgs = [img1, img2, img3, img4]
+
+for i in range(4):
+    plt.subplot(2,2,i+1)
+    plt.imshow(imgs[i])
+    plt.title(title[i])
+    plt.xticks([])
+    plt.yticks([])
+plt.show()
